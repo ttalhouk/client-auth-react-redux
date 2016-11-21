11f5fe3b7491550bd2135cc298aee6b568041e3f
Talal Talhouk <talal@sbcglobal.net>
2016-11-21 12:31:48 -0800

    update signin form and action creators

---
+++ src/actions/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, AUTH_ERROR } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
+
+
+export function signinUser({ email, password }) {
+  // thunk allows for multiple dispatching
+  return function(dispatch) {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
+      });
+  }
+}
+
+// ERROR ACTION CREATOR
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
+};
