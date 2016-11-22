d083d9960df35b5518937b943270bcccb49b1a52
Talal Talhouk <talal@sbcglobal.net>
2016-11-22 09:25:30 -0800

    complete signup authentication

--- src/actions/index.js
+++ src/actions/index.js
@@ -41,3 +41,25 @@ export function signoutUser() {
   localStorage.removeItem('token');
   return({ type: UNAUTH_USER });
 }
+
+export function signupUser({ email, password }) {
+  // thunk allows for multiple dispatching
+  return function(dispatch) {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+        dispatch(authError(response.data.error));
+      });
+  }
+}
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,9 +4,10 @@ import * as actions from '../../actions';
 
 
 class Signup extends Component {
-  handleFormSubmit({ email, password }) {
+
+  handleFormSubmit(formProps) {
     // Need to do something to log user in
-    this.props.signinUser({ email, password });
+    this.props.signupUser(formProps);
   }
 
   renderAlert() {
@@ -27,14 +28,17 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input {...email} className="form-control" />
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
           <input {...password} type="password" className="form-control" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password Confirmation:</label>
           <input {...passwordConfirm} type="password" className="form-control" />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
         {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign in</button>
@@ -50,7 +54,29 @@ function mapStateToProps(state) {
   };
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = 'please enter an email';
+  }
+  if (!formProps.password) {
+    errors.password = 'please enter an password';
+  }
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'please enter an Password Confirmation';
+  }
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate: valadate
+
 }, mapStateToProps, actions)(Signup);
